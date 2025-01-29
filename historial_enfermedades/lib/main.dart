e7712f838bf290fcbcf2584e99fdd8f3481c631b
ricardoherreral <l19120189@morelia.tecnm.mx>
2025-01-28 21:16:18 -0600

    chore(main): add manual insert to box and init objectbox

--- historial_enfermedades/lib/main.dart
+++ historial_enfermedades/lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:historial_enfermedades/models/recipe.dart';
 import 'package:historial_enfermedades/pages/log_in.dart';
+import 'package:historial_enfermedades/tools/objectBoxHelper.dart';
+import 'package:path_provider/path_provider.dart'; // Importa path_provider
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+   final directory = await getApplicationDocumentsDirectory();
+  WidgetsFlutterBinding.ensureInitialized();
+  await ObjectBoxHelper.init(directory.path);
+  final recipe = Recipe(
+  date: DateTime.now(),
+  pacient: "Pepe Perez",
+  doctor: "Dr. Guzman",
+  phone: "5551234567",
+  discomfort: "Dolor de cabeza",
+  img: "path/to/image.jpg",
+  );
+  ObjectBoxHelper.recipeBox.put(recipe);
   runApp(const MyApp());
 }
 
