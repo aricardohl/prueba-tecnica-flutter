482002a2807b874695dae921bb289333764b569b
ricardoherreral <l19120189@morelia.tecnm.mx>
2025-01-30 21:11:31 -0600

    chore(dependencies): add dependencies for use camera, set up android manifest

--- historial_enfermedades/macos/Flutter/GeneratedPluginRegistrant.swift
+++ historial_enfermedades/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import gal
 import objectbox_flutter_libs
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  GalPlugin.register(with: registry.registrar(forPlugin: "GalPlugin"))
   ObjectboxFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "ObjectboxFlutterLibsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
