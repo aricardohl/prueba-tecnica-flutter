d1ea3c73bd4e3c0f7c2905af73cd7e90611050cd
ricardoherreral <l19120189@morelia.tecnm.mx>
2025-01-31 10:25:19 -0600

    fix(login): refactor code

--- historial_enfermedades/lib/pages/log_in.dart
+++ historial_enfermedades/lib/pages/log_in.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:historial_enfermedades/constants/strings.dart';
-import 'package:historial_enfermedades/pages/_listado.dart';
-import 'package:historial_enfermedades/pages/widgets/errorWidget.dart';
+import 'package:historial_enfermedades/pages/widgets/text_field.dart';
+import 'package:historial_enfermedades/pages/widgets/text_field_pass.dart';
+import 'package:historial_enfermedades/utils/validators.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -16,23 +17,12 @@ class _LoginPageStateClass extends State<Login> {
   late double _deviceHeight;
   late double _deviceWidth;
 
-  final emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
   //Por esto es clase Stateful
   String? _newEmail;
   String? _newPassword;
-  String? _validateEmail(String? value) {
-    if (value == null || value.isEmpty) {
-      return AppStrings.emailError;
-    }
-    if (!emailRegex.hasMatch(value)) {
-      return AppStrings.validEmailError;
-    } else {
-      return null;
-    }
-  }
 
   _LoginPageStateClass();
 
@@ -51,33 +41,6 @@ class _LoginPageStateClass extends State<Login> {
     _newEmail = emailController.text;
   }
 
-  Widget textFieldEmail(BuildContext context) {
-    return Center(
-        child: TextField(
-          maxLength: 100,
-          controller: emailController,
-          decoration: InputDecoration(
-              counterText: '',
-              border: OutlineInputBorder(),
-              labelText: AppStrings.labelEmail,
-              hintText: AppStrings.hintEmail),
-        ));
-  }
-
-  Widget textFieldPassword(BuildContext context) {
-    return Center(
-        child: TextField(
-          maxLength: 100,
-          controller: passwordController,
-          obscureText: true,
-          decoration: InputDecoration(
-              counterText: '',
-              border: OutlineInputBorder(),
-              labelText: AppStrings.labelPassword,
-              hintText: AppStrings.hintPassword),
-        ));
-  }
-
   Widget buttonIniciarSesion(BuildContext context) {
     return Center(
         child: TextButton(
@@ -85,31 +48,11 @@ class _LoginPageStateClass extends State<Login> {
                 backgroundColor: WidgetStatePropertyAll<Color>(
                     Color.fromARGB(255, 4, 65, 116))),
             onPressed: () {
-              handleButtonLogin();
+              handleButtonLogin(context, _newEmail, _newPassword);
             },
-            child:
-                Text(style: TextStyle(color: Colors.white), AppStrings.logInText)));
-  }
-
-  bool handleButtonLogin() {
-    if(_newEmail == null || _newEmail!.isEmpty || _newPassword == null || _newPassword!.isEmpty) {
-      toastErrorMessage(context, AppStrings.sendEmailPasswordError);
-      return false;
+            child: Text(
+                style: TextStyle(color: Colors.white), AppStrings.logInText)));
     }
-    final emailErrorMsg = _validateEmail(_newEmail);
-    if(emailErrorMsg != null) {
-      toastErrorMessage(context, emailErrorMsg);
-      return false;
-    }
-    if (_newEmail == 'jhon@mail.com' && _newPassword == "77@1\$") {
-      Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => ListadoPage()));
-    } else {
-      toastErrorMessage(context, AppStrings.mailPasswordError);
-    }
-    return false;
-  }
-
 
   @override
   Widget build(BuildContext context) {
@@ -126,22 +69,21 @@ class _LoginPageStateClass extends State<Login> {
         ),
         body: SafeArea(
             child: Container(
-          height: _deviceHeight,
-          width: _deviceWidth,
-          padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.08),
-          child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(height: 20),
-                  textFieldEmail(context),
-                  SizedBox(height: 20),
-                  textFieldPassword(context),
-                  SizedBox(height: 20),
-                  buttonIniciarSesion(context),
-                ],
-              )
-        )
-      )
-    );
+                height: _deviceHeight,
+                width: _deviceWidth,
+                padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.08),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 20),
+                    textFieldIniciarSesion(context, emailController,
+                        AppStrings.labelEmail, AppStrings.hintEmail),
+                    SizedBox(height: 20),
+                    textFieldPassword(context, passwordController,
+                        AppStrings.labelPassword, AppStrings.hintPassword),
+                    SizedBox(height: 20),
+                    buttonIniciarSesion(context),
+                  ],
+                ))));
   }
 }
